40c2d1191339306d549a28c08bce671a3b946f10
Juan <juangabrielsa147@gmail.com>
2022-09-13 17:15:30 -0300

    Correções e adição da barra de especial.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [damage, setDamage] = useState(false);
   const [life, setLife] = useState(10);
   const [currentEnemy, setCurrentEnemy] = useState([]);
+  const [specialCount, setSpecialCount] = useState(0);
   const animateSpecialIcon = useAnimation();
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
@@ -22,8 +23,17 @@ function App() {
   useEffect(() => {
     if (state.specialAttack)
       animateSpecialAttack()
-
   }, [state.specialAttack])
+
+  useEffect(() => {
+    if (specialCount < 250) {
+      const specialInterval = setInterval(() => {
+        setSpecialCount(prevState => prevState + 1)
+      }, 10)
+
+      return () => clearInterval(specialInterval)
+    }
+  }, [specialCount])
 
   useEffect(() => {
     if (life <= 999 && heartCount < 1) {
@@ -80,18 +90,29 @@ function App() {
   }, [state.characterStamina])
   return (
     <div className="game-content">
+
+      {/* Lifebar */}
       <div className='life-bar' style={{ opacity: state.specialAttack ? 0.4 : 1 }}>
-        <div className='life-bar-icon' />
+        <div className='life-bar-text'>
+          Saúde
+        </div>
         <div className='life-bar-out' />
         <div className='life-bar-in' style={{
           width: life * 27.1
         }} />
       </div>
-      <div className='vigor-bar-out' style={{
-        opacity: state.specialAttack ? 0.4 : 1,
-      }}>
-      </div>
-      <motion.div className='vigor-bar-in'
+      {/* Lifebar */}
+
+      {/* Stamina */}
+      <div className='vigor-bar'>
+        <div className='vigor-bar-text'>
+          Vigor
+        </div>
+        <div className='vigor-bar-out' style={{
+          opacity: state.specialAttack ? 0.4 : 1,
+        }}>
+        </div>
+        <motion.div className='vigor-bar-in'
           style={{
             width: state.characterStamina
           }}
@@ -108,6 +129,44 @@ function App() {
             } :
             { opacity: 1 }}
         />
+      </div>
+      {/* Stamina */}
+      {/* Especial */}
+      <div className='special-bar' style={{ opacity: state.specialAttack ? 0.4 : 1 }}>
+        <div className='special-bar-text'>
+          Mil cortes
+        </div>
+        <motion.div className='special-bar-out' />
+        <motion.div className='special-bar-in' style={{
+          width: specialCount,
+          background: specialCount == 250 ? 'red' : 'linear-gradient(90deg, rgba(0,18,181,1) 0%, rgba(0,12,120,1) 100%)'
+        }}
+          animate={
+            specialCount == 250 &&
+            {
+              opacity: [0, 1, 0],
+              transition: {
+                repeat: Infinity,
+                ease: 'linear',
+                duration: 0.6
+              }
+            }
+          } />
+        {specialCount == 250 &&
+          <motion.h1 className='special-icon' animate={{
+            x: [2, 0, 2],
+            opacity: [0, 1, 0],
+            transition: {
+              repeat: Infinity,
+              ease: 'linear',
+              duration: 0.1
+            }
+          }}>
+            危
+          </motion.h1>
+        }
+      </div>
+      {/* Especial */}
       <Character damage={damage} onDamage={() => setLife(prevState => prevState - 1)} />
       {/* <Boss isAttacking={(e) => setDamage(e)} onDeath={() => setEnemyCount(prevState => prevState + 1)} /> */}
       <motion.h1
